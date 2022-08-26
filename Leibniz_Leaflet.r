886d0b261583d605e8e7b10930049c55036a85fb
stefano della chiesa <stefano.dellachiesa@gmail.com>
2022-08-26 15:08:19 +0200

    edited input file and script

--- Leibniz_Leaflet.r
+++ Leibniz_Leaflet.r
@@ -7,10 +7,12 @@ library(leaflet)
 df<-read.csv(file='./Leibniz_Instittues_State_GroupDomain.csv', sep=";")
 df$lat = as.numeric(format(df$lat, digits = 9))
 df$lon = as.numeric(format(df$lon, digits = 9))
-# --- Data table to be showed in ui
-df_table<-df[c(1,2,3,4,5)]
 
-#Plot the points
+# --- Data table to be showed in ui
+df_table<-df[c(1,2,3,4,5,6)]
+
+
+
 leaflet(df_table) %>% addTiles() %>%
   addCircleMarkers(lng = ~lon, lat = ~lat,
                    popup = paste("<b>INSTITUTE NAME</b>",
@@ -19,12 +21,16 @@ leaflet(df_table) %>% addTiles() %>%
                     paste("<b>DOMAIN</b>","<br>",df_table$Domain)))
 
 
-# #Plot the points
-# leaflet(df_table) %>% addTiles() %>%
-#   addCircleMarkers(lng = ~lon, lat = ~lat, 
-#                    popup = paste("INSTITUTE NAME",
-#                                  "<br>",df_table$Name,"<br>",
-#                                  paste("FEDERAL STATE","<br>",df_table$FederalState),"<br>",
-#                                  paste("DOMAIN","<br>",df_table$Domain)))
+
+#, clusterOptions = markerClusterOptions()
+
+
+ #Plot the points
+ leaflet(df_table) %>% addTiles() %>%
+   addMarkers(lng = ~lon, lat = ~lat,
+                    popup = paste("INSTITUTE NAME",
+                                  "<br>",df_table$Name,"<br>",
+                                  paste("FEDERAL STATE","<br>",df_table$FederalState),"<br>",
+                                  paste("DOMAIN","<br>",df_table$Domain)))
 
 # Location of the Leibniz Institute  - field domain Economics, Social Sciences and Spatial Research
